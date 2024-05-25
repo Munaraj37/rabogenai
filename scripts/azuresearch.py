d00e10e37918977d7ff365112aa74312678f7d71
munaraj37 <mg.rabobank@example.com>
2024-05-25 13:56:41 +0200

    Updated Scripts

--- scripts/azuresearch.py
+++ scripts/azuresearch.py
@@ -1,18 +1,33 @@
 
 import os
-
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
 from azure.search.documents.indexes import SearchIndexClient
-from azure.search.documents.models import VectorizedQuery
-from azure.search.documents.models import Vector
 
-service_endpoint = 'https://acsrabogenai.search.windows.net' #os.environ["AZURE_SEARCH_SERVICE_ENDPOINT"]
-index_name = 'vrm-index' #os.environ["AZURE_SEARCH_INDEX_NAME"]
-key = 'eIvQly2BQv7MpjhhmCH0WYI7fBcqlIweVLecds5QLFAzSeBbtGBa' #os.environ["AZURE_SEARCH_API_KEY"]
+# Environment variables
+# Speech resource (required)
+speech_region = os.environ.get('SPEECH_REGION') # e.g. westus2
+speech_key = os.environ.get('SPEECH_KEY')
+speech_private_endpoint = os.environ.get('SPEECH_PRIVATE_ENDPOINT') # e.g. https://my-speech-service.cognitiveservices.azure.com/ (optional)
 
-OpenAIEndpoint = 'https://aoairabogenai.openai.azure.com/'
-OpenAIKey = 'b1d16fbce07644668935410f4c54f74e'
+# OpenAI resource (required for chat scenario)
+azure_openai_endpoint = os.environ.get('AZURE_OPENAI_ENDPOINT') # e.g. https://my-aoai.openai.azure.com/
+azure_openai_api_key = os.environ.get('AZURE_OPENAI_API_KEY')
+azure_openai_deployment_name = os.environ.get('AZURE_OPENAI_DEPLOYMENT_NAME') # e.g. my-gpt-35-turbo-deployment
+azure_openai_embed_deployment_name = os.environ.get('AZURE_OPENAI_EMBED_DEPLOYMENT_NAME') # e.g. my-gpt-35-turbo-deployment
+
+
+# Cognitive search resource (optional, only required for 'on your data' scenario)
+cognitive_search_endpoint = os.environ.get('COGNITIVE_SEARCH_ENDPOINT') # e.g. https://my-cognitive-search.search.windows.net/
+cognitive_search_api_key = os.environ.get('COGNITIVE_SEARCH_API_KEY')
+cognitive_search_index_name = os.environ.get('COGNITIVE_SEARCH_INDEX_NAME') # e.g. my-search-index
+
+#service_endpoint = 'https://acsrabogenai.search.windows.net' #os.environ["AZURE_SEARCH_SERVICE_ENDPOINT"]
+#index_name = 'vrm-index' #os.environ["AZURE_SEARCH_INDEX_NAME"]
+#key = 'eIvQly2BQv7MpjhhmCH0WYI7fBcqlIweVLecds5QLFAzSeBbtGBa' #os.environ["AZURE_SEARCH_API_KEY"]
+
+#OpenAIEndpoint = 'https://aoairabogenai.openai.azure.com/'
+#OpenAIKey = 'b1d16fbce07644668935410f4c54f74e'
 
 def get_document_info():
 
@@ -44,18 +59,14 @@ def get_document_info():
 
 
 def get_embeddings(text: str):
-    # There are a few ways to get embeddings. This is just one example.
     import openai
 
-    open_ai_endpoint = OpenAIEndpoint #os.getenv("OpenAIEndpoint")
-    open_ai_key = OpenAIKey #os.getenv("OpenAIKey")
-
     client = openai.AzureOpenAI(
-        azure_endpoint=open_ai_endpoint,
-        api_key=open_ai_key,
+        azure_endpoint=azure_openai_endpoint,
+        api_key=azure_openai_api_key,
         api_version="2023-09-01-preview",
     )
-    embedding = client.embeddings.create(input=[text], model="embedding-rabogenai")
+    embedding = client.embeddings.create(input=[text], model=azure_openai_embed_deployment_name)
     return embedding.data[0].embedding
 
 
@@ -87,19 +98,17 @@ def get_hotel_index(name: str):
         profiles=[VectorSearchProfile(name="my-vector-config", algorithm_configuration_name="my-algorithms-config")],
         algorithms=[HnswAlgorithmConfiguration(name="my-algorithms-config")],
     )
+   
     return SearchIndex(name=name, fields=fields, vector_search=vector_search)
 
 
 if __name__ == "__main__":
     print("Started")
-    credential = AzureKeyCredential(key)
-    index_client = SearchIndexClient(service_endpoint, credential)
-    index = get_hotel_index(index_name)
+    index_client = SearchIndexClient(cognitive_search_endpoint, AzureKeyCredential(cognitive_search_api_key))
+    index = get_hotel_index(cognitive_search_index_name)
     index_client.create_index(index)
-    print("Index Config")
-    client = SearchClient(service_endpoint, index_name, credential)
+    client = SearchClient(cognitive_search_endpoint, cognitive_search_index_name, AzureKeyCredential(cognitive_search_api_key))
     hotel_docs = get_document_info()
     print("Doc Loaded")
-    #print(hotel_docs)
     client.upload_documents(documents=hotel_docs)
     print("Completed")
